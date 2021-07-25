75f7f7870fbb64196232aa5620c081ba5a724b27
Andrii Mishchenko <a.mishenko2001@gmail.com>
2021-07-25 16:36:34 +0300

    Updates

---
+++ static/js/6.86a49082.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[6],{101:function(t,e,o){"use strict";o.r(e);o(0);var n=o(87),i=o.n(n),a=o(1);e.default=function(){return Object(a.jsx)("h1",{className:i.a.title,children:"404 - Page not found"})}},87:function(t,e,o){t.exports={container:"HomeView_container__1iJ0a",title:"HomeView_title__206Pu"}}}]);
+//# sourceMappingURL=6.86a49082.chunk.js.map
